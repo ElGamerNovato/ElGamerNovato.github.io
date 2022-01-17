33bb4d9f389fd323acc1a542781717d6c47abcef
Cristopher <criscruflo@gmail.com>
2022-01-17 15:59:39 -0500

    Segundo commit. En esta ocasión, arreglé el error que hacía que el html no tenga límite en Y

--- js/app.js
+++ js/app.js
@@ -4,13 +4,17 @@ let mountains_behind=document.querySelector('#mountains_behind');
 let text=document.querySelector('#text');
 let btn= document.querySelector('#btn')
 let mountains_front=document.querySelector('#mountains_front');
+let header= document.querySelector('header')
 window.addEventListener('scroll', function(){
     let value=window.scrollY;
-    stars.style.left=value*0.25+'px';
-    moon.style.top=value*1.25+'px';
-    mountains_behind.style.top=value*0.5+'px';
-    text.style.marginRight=value*3.5+'px';
-    text.style.marginTop=value+'px'
-    btn.style.marginTop=value+'px'
+    if(value<=this.screen.height){
+        stars.style.left=value*0.25+'px';
+        moon.style.top=value*1.25+'px';
+        mountains_behind.style.top=value*0.5+'px';
+        text.style.marginRight=value*3.5+'px';
+        text.style.marginTop=value+'px'
+        btn.style.marginTop=value+'px'
+        header.style.top=value*0.8+'px'
+    }
 })
 
