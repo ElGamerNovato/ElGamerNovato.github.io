5ba52e5525bc7b3dddaf76ff51536455931bd372
Cristopher <criscruflo@gmail.com>
2022-01-17 16:46:33 -0500

    Es el cuarto commit. Acá hice la página web responsive

--- js/app.js
+++ js/app.js
@@ -7,14 +7,27 @@ let mountains_front=document.querySelector('#mountains_front');
 let header= document.querySelector('header')
 window.addEventListener('scroll', function(){
     let value=window.scrollY;
-    if(value<=this.window.innerHeight){
-        stars.style.left=value*0.25+'px';
-        moon.style.top=value*1.25+'px';
-        mountains_behind.style.top=value*0.5+'px';
-        text.style.marginRight=value*3.5+'px';
-        text.style.marginTop=value+'px'
-        btn.style.marginTop=value+'px'
-        header.style.top=value*0.8+'px'
+    if(this.window.matchMedia("(min-width: 600px)").matches){
+        if(value<=this.window.innerHeight-200){
+            stars.style.left=value*0.25+'px';
+            moon.style.top=value*1.25+'px';
+            mountains_behind.style.top=value*0.5+'px';
+            text.style.marginRight=value*3.5+'px';
+            text.style.marginTop=value+'px'
+            btn.style.marginTop=value+'px'
+            header.style.top=value*0.8+'px'
+    } 
+    }else{
+        if(value<=this.window.innerHeight){
+            stars.style.left=value*0.15+'px';
+            moon.style.top=value+'px';
+            mountains_behind.style.top=value*0.1+'px';
+            text.style.marginRight=value*2+'px';
+            text.style.marginTop=value*0.5+'px'
+            btn.style.marginTop=value+'px'
+            header.style.top=value*0.5+'px'        
     }
+}
+console.log(`window: ${this.window.innerHeight}`)
+console.log(`value: ${value}`)
 })
-
